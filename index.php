8bd20d67ec722cad4097fb37de8c9275ccea6ea1
HimeLiv <liviodelenclos19@gmail.com>
2025-08-15 10:56:43 +0200

    mise en place du index et de l'htaccess

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php 
+
+// Appel du fichier contenant les excuses pour qu'il soit accessible dans partout
+include(__DIR__ . '/data/excuses.php');
+
+$route = $_GET['route'] ?? 'home';
+
+switch ($route) {
+    case 'home':
+        include(__DIR__ . '/views/home.php');
+        break;
+    case 'lost':
+        include(__DIR__ . '/views/lost.php');
+        break;
+    case 'http_code':
+        $code = $_GET['code'] ?? '000';
+        include(__DIR__ . '/views/http_code.php');
+        break;
+    case '404':
+        default:
+            include(__DIR__ . '/views/404.php');
+            break;
+}
---
+++ views/lost.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>I’m lost</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Redirection automatique après 5 secondes -->
+    <meta http-equiv="refresh" content="5;url=/">
+</head>
+
+<body class="bg-light d-flex justify-content-center align-items-center vh-100">
+
+    <!-- Conteneur du gif -->
+    <div class="text-center">
+        <img src="/public/images/lost.gif" class="img-fluid" alt="I'm lost">
+        <p class="mt-3 fw-bold">I’m lost...</p>
+    </div>
+
+</body>
+
+</html>
