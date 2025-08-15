aef9b6b07965a1fbe26c2c636b006b6add6b4e85
HimeLiv <liviodelenclos19@gmail.com>
2025-08-15 10:32:01 +0200

    création des fichiers qui me semblent utiles, et mise en place du tableau d'excuses

---
+++ data/excuses.php
@@ -0,0 +1,20 @@
+<?php 
+
+$excuses = [
+    "Mon chat a changé mon mot de passe et refuse de me le redonner.",
+    "Je suis en retard, mon grille-pain a déclaré la guerre à mon micro-ondes.",
+    "Mon réveil a fait grève.",
+    "Meh.",
+    "I wrote the code and missed the necessary validation by an oversight.",
+    "It works on my machine.", 
+    "It's a feature, not a bug.",
+    "It works in my timezone.",
+    "I swear the bug wasn’t there before I pushed.",
+    "My code is just too advanced for Git to understand.",
+    "I only work in dark mode, light mode breaks everything.",
+    "My rubber duck disagreed with my logic.",
+    "Stack Overflow told me to do it.",
+    "The logs are lying to you.",
+    "My code only works after midnight.",
+    "It works only when nobody watches."
+];
